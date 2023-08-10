9222c006d5b4ec1cd274657f4ad97dee7fac763a
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-08-10 05:52:55 -0400

    great

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -24,3 +24,19 @@ class BasicAuth(Auth):
             return base64_part
         else:
             return None
+
+    def decode_base64_authorization_header(
+                                           self,
+                                           base64_authorization_header:
+            str) -> str:
+        """ returns the decoded value of a Base64 string"""
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            decoded_str = base64.b64decode(base64_authorization_header)
+            .decode('utf-8')
+            return decoded_str
+        except base64.binascii.Error:
+            return None
