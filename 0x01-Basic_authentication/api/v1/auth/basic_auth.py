6be226db6bcfc0c535788aa6d675c67985578154
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-08-10 07:46:24 -0400

    greata

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 """
-class BasicAuth that inherits from Auth
+BasicAuth class inherits from Auth
 """
 from api.v1.auth.auth import Auth
 import base64
 
 
 class BasicAuth(Auth):
-    """Create a class BasicAuth that inherits from Auth"""
-
+    """
+    Create a class BasicAuth that inherits from Auth
+    """
     def extract_base64_authorization_header(
             self,
             authorization_header: str) -> str:
-        """ returns the Base64 part of the Authorization header
-        for a Basic Authentication:"""
-
+        """returns the Base64 part of the Authorization header
+        for a Basic Authentication
+        """
         if authorization_header is None:
             return None
         if type(authorization_header) is not str:
@@ -30,7 +31,7 @@ class BasicAuth(Auth):
             self,
             base64_authorization_header: str) -> str:
         """ returns the decoded value of a Base64 string
-        base64_authorization_header:"""
+        base64_authorization_header"""
 
         if base64_authorization_header is None:
             return None
