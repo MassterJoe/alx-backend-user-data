19f5627eb017b65ea9194bc5668842102051b16c
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-08-10 10:16:57 -0400

    finally

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -17,6 +17,8 @@ class Auth:
         for i in excluded_paths:
             if i == path+'/' or i == path or path in excluded_paths:
                 return False
+            if i == path+'*' in excluded_paths:
+                return False
             return True
 
     def authorization_header(self, request=None) -> str:
