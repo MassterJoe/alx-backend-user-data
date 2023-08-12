2221b0ab2fe7e676b0be89ff90aece3232937834
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-08-12 18:17:21 -0400

    cool

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+A new Flask view that handles all routes for
+ the Session authentication.
+"""
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models.user import User
+import os
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """ Login route handler"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email is None or email == "":
+        return jsonify({"error": "email missing"}), 400
+    if password is None or password == "":
+        return jsonify({"error": "password missing"}), 400
+    users = User.search({'email': email})
+    if (len(users) <= 0):
+        return jsonify({"error": "no user found for this email"}), 404
+    if not users[0].is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+    from api.v1.app import auth
+    session_id = auth.create_session(user.id)
+    cookie_name = os.environ.get('SESSION_NAME')
+    response = jsonify(user.to_json())
+    response.set_cookie(cookie_name, session_id)
+    return response
