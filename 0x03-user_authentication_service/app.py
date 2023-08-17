b3919f796463cc4489c3384e7b192618db74b2fe
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-08-17 16:58:56 -0400

    cool

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -48,5 +48,18 @@ def login() -> str:
     return response
 
 
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """ logout """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        response = jsonify({'message': 'logout successful'})
+        response.delete_cookie('session_id')
+        return redirect('/', code=302)
+    else:
+        abort(403)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
