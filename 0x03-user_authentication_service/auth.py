ca8b686dbf7282c02f9d54d8343199327cd7da10
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-08-17 16:42:51 -0400

    grrat

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -86,7 +86,8 @@ class Auth:
 
     def destroy_session(self, user_id: int) -> None:
         """ The method takes a single user_id integer
-        argument and returns None."""
+        argument and returns None.
+        """
         if user_id is None:
             return None
-        return self._id.update_user(user_id, session_id=None)
+        self._id.update_user(user_id, session_id=None)
