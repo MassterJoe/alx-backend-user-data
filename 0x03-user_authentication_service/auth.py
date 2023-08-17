d5293867667cca00be65a88a3dfcdc9ab2591ee0
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-08-17 14:10:13 -0400

    cool

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -57,3 +57,14 @@ class Auth:
                 return False
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """ It takes an email string argument
+        returns the session ID as a string."""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
