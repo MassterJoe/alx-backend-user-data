cc875716e942826aa1408c572232c5624ff0be2f
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-08-17 15:33:02 -0400

    cool

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,8 +3,9 @@
 Create a Flask app that has a
 single GET route ("/")
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
+from sqlalchemy.orm.exc import NoResultFound
 
 
 app = Flask(__name__)
@@ -29,6 +30,24 @@ def users():
                      "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """ login"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not AUTH.valid_login(email, password):
+        abort(404)
+    else:
+        session_id = AUTH.create_session(email)
+        response = jsonify(
+                           {"email": f'{email}',
+                            "message": "logged in"}
+        )
+        response.set_cookie("session_id", session_id)
+        return response
 
 
 if __name__ == "__main__":
