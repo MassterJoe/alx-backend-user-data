fb640eae8cb55c064b345f36995e52d4b868562c
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-08-17 13:51:13 -0400

    nice

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -19,7 +19,7 @@ def _hash_password(password: str) -> bytes:
     return hashed_password
 
 
-def _generate_uuid():
+def _generate_uuid() -> str:
     """return a string representation of a new UUID
     """
     new_uuid = uuid.uuid4()
